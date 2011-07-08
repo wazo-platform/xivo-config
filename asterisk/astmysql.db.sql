a33c3d4ada15b8bc434ee0e63b034eedbddefcf6
Cédric Abunar <cabunar@proformatique.com>
2011-07-08 19:21:12 +0000

    remove ctiprofilesgroup table fix webservices callcenter for ctiserver configuration
    git-svn-id: https://rcs.lan.proformatique.com/svn/xivo/trunk@11241 dc85baef-7018-0410-bfbd-8a639da648bb

--- asterisk/astmysql.db.sql
+++ asterisk/astmysql.db.sql
@@ -457,7 +457,6 @@ INSERT INTO `ctipresences` VALUES(1,'xivo','De base non supprimable',0);
 DROP TABLE IF EXISTS `ctiprofiles`;
 CREATE TABLE `ctiprofiles` (
  `id` int(10) unsigned auto_increment,
- `idgroup` int(10) unsigned,
  `xlets` text,
  `funcs` varchar(255),
  `maxgui` int(10) unsigned,
@@ -470,25 +469,14 @@ CREATE TABLE `ctiprofiles` (
  PRIMARY KEY(`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-INSERT INTO `ctiprofiles` VALUES(9,1,'[[ `queues`, `dock`, `fms`, `N/A` ],[ `queuedetails`, `dock`, `fms`, `N/A` ],[ `queueentrydetails`, `dock`, `fcms`, `N/A` ],[ `agents`, `dock`, `fcms`, `N/A` ],[ `agentdetails`, `dock`, `fcms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `conference`, `dock`, `fcm`, `N/A` ]]','agents,presence,switchboard',-1,'Superviseur','agentsup','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(10,1,'[[ `queues`, `dock`, `ms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `customerinfo`, `dock`, `cms`, `N/A` ],[ `agentdetails`, `dock`, `cms`, `N/A` ]]','presence',-1,'Agent','agent','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(11,1,'[[ `tabber`, `grid`, `fcms`, `1` ],[ `dial`, `grid`, `fcms`, `2` ],[ `search`, `tab`, `fcms`, `0` ],[ `customerinfo`, `tab`, `fcms`, `4` ],[ `identity`, `grid`, `fcms`, `0` ],[ `fax`, `tab`, `fcms`, `N/A` ],[ `history`, `tab`, `fcms`, `N/A` ],[ `directory`, `tab`, `fcms`, `N/A` ],[ `features`, `tab`, `fcms`, `N/A` ],[ `mylocaldir`, `tab`, `fcms`, `N/A` ],[ `conference`, `tab`, `fcms`, `N/A` ]]','presence,customerinfo',-1,'Client','client','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(12,1,'[[ `tabber`, `grid`, `fcms`, `1` ],[ `dial`, `grid`, `fcms`, `2` ],[ `search`, `tab`, `fcms`, `0` ],[ `customerinfo`, `tab`, `fcms`, `4` ],[ `identity`, `grid`, `fcms`, `0` ],[ `fax`, `tab`, `fcms`, `N/A` ],[ `history`, `tab`, `fcms`, `N/A` ],[ `directory`, `tab`, `fcms`, `N/A` ],[ `features`, `tab`, `fcms`, `N/A` ],[ `mylocaldir`, `tab`, `fcms`, `N/A` ],[ `conference`, `tab`, `fcms`, `N/A` ],[ `outlook`, `tab`, `fcms`, `N/A` ]]','presence,customerinfo',-1,'Client+Outlook','clientoutlook','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(13,1,'[[ `datetime`, `dock`, `fm`, `N/A` ]]','',-1,'Horloge','clock','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(14,1,'[[ `dial`, `dock`, `fm`, `N/A` ],[ `operator`, `dock`, `fcm`, `N/A` ],[ `datetime`, `dock`, `fcm`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `calls`, `dock`, `fcm`, `N/A` ],[ `parking`, `dock`, `fcm`, `N/A` ]]','presence,switchboard,search,dial',-1,'Opérateur','oper','xivo','','',1);
-INSERT INTO `ctiprofiles` VALUES(15,1,'[[ `parking`, `dock`, `fcms`, `N/A` ],[ `search`, `dock`, `fcms`, `N/A` ],[ `calls`, `dock`, `fcms`, `N/A` ],[ `switchboard`, `dock`, `fcms`, `N/A` ],[ `customerinfo`, `dock`, `fcms`, `N/A` ],[ `datetime`, `dock`, `fcms`, `N/A` ],[ `dial`, `dock`, `fcms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `operator`, `dock`, `fcms`, `N/A` ]]','switchboard,dial,presence,customerinfo,search,agents,conference,directory,features,history,fax,chitchat,database','','Switchboard','switchboard','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(9,'[[ `queues`, `dock`, `fms`, `N/A` ],[ `queuedetails`, `dock`, `fms`, `N/A` ],[ `queueentrydetails`, `dock`, `fcms`, `N/A` ],[ `agents`, `dock`, `fcms`, `N/A` ],[ `agentdetails`, `dock`, `fcms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `conference`, `dock`, `fcm`, `N/A` ]]','agents,presence,switchboard',-1,'Superviseur','agentsup','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(10,'[[ `queues`, `dock`, `ms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `customerinfo`, `dock`, `cms`, `N/A` ],[ `agentdetails`, `dock`, `cms`, `N/A` ]]','presence',-1,'Agent','agent','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(11,'[[ `tabber`, `grid`, `fcms`, `1` ],[ `dial`, `grid`, `fcms`, `2` ],[ `search`, `tab`, `fcms`, `0` ],[ `customerinfo`, `tab`, `fcms`, `4` ],[ `identity`, `grid`, `fcms`, `0` ],[ `fax`, `tab`, `fcms`, `N/A` ],[ `history`, `tab`, `fcms`, `N/A` ],[ `directory`, `tab`, `fcms`, `N/A` ],[ `features`, `tab`, `fcms`, `N/A` ],[ `mylocaldir`, `tab`, `fcms`, `N/A` ],[ `conference`, `tab`, `fcms`, `N/A` ]]','presence,customerinfo',-1,'Client','client','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(12,'[[ `tabber`, `grid`, `fcms`, `1` ],[ `dial`, `grid`, `fcms`, `2` ],[ `search`, `tab`, `fcms`, `0` ],[ `customerinfo`, `tab`, `fcms`, `4` ],[ `identity`, `grid`, `fcms`, `0` ],[ `fax`, `tab`, `fcms`, `N/A` ],[ `history`, `tab`, `fcms`, `N/A` ],[ `directory`, `tab`, `fcms`, `N/A` ],[ `features`, `tab`, `fcms`, `N/A` ],[ `mylocaldir`, `tab`, `fcms`, `N/A` ],[ `conference`, `tab`, `fcms`, `N/A` ],[ `outlook`, `tab`, `fcms`, `N/A` ]]','presence,customerinfo',-1,'Client+Outlook','clientoutlook','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(13,'[[ `datetime`, `dock`, `fm`, `N/A` ]]','',-1,'Horloge','clock','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(14,'[[ `dial`, `dock`, `fm`, `N/A` ],[ `operator`, `dock`, `fcm`, `N/A` ],[ `datetime`, `dock`, `fcm`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `calls`, `dock`, `fcm`, `N/A` ],[ `parking`, `dock`, `fcm`, `N/A` ]]','presence,switchboard,search,dial',-1,'Opérateur','oper','xivo','','',1);
+INSERT INTO `ctiprofiles` VALUES(15,'[[ `parking`, `dock`, `fcms`, `N/A` ],[ `search`, `dock`, `fcms`, `N/A` ],[ `calls`, `dock`, `fcms`, `N/A` ],[ `switchboard`, `dock`, `fcms`, `N/A` ],[ `customerinfo`, `dock`, `fcms`, `N/A` ],[ `datetime`, `dock`, `fcms`, `N/A` ],[ `dial`, `dock`, `fcms`, `N/A` ],[ `identity`, `grid`, `fcms`, `0` ],[ `operator`, `dock`, `fcms`, `N/A` ]]','switchboard,dial,presence,customerinfo,search,agents,conference,directory,features,history,fax,chitchat,database','','Switchboard','switchboard','xivo','','',1);
 
-
-DROP TABLE IF EXISTS  `ctiprofilesgroup `;
-CREATE TABLE  `ctiprofilesgroup ` (
-  `id` SERIAL,
-  `name` varchar(255),
-  `description` varchar(255),
-  `deletable` tinyint(1),
- PRIMARY KEY( `id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-INSERT INTO  `ctiprofilesgroup` VALUES(1,'xivo','De base non supprimable',0);
 
 DROP TABLE IF EXISTS `ctireversedirectories`;
 CREATE TABLE `ctireversedirectories` (
