0e74e93e559b36c92718ae73cdacb2aed84b043c
Etienne Lessard <edesrocherslessard@avencall.com>
2011-11-23 10:21:38 -0500

    Update copyright from Proformatique to Avencall in base-config.

--- asterisk/astpgsql.db.sql
+++ asterisk/astpgsql.db.sql
@@ -1,6 +1,6 @@
 /*
  * XiVO Base-Config
- * Copyright (C) 2006-2011  Proformatique <technique@proformatique.com>
+ * Copyright (C) 2006-2011  Avencall
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
