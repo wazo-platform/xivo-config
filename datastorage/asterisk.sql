95e1eb3303bc6b99720583fc4b52d7e0c468359d
Pascal Cadotte-Michaud <pcadottemichaud@proformatique.com>
2012-08-16 09:37:40 -0400

    [evol] Add indexes to stat_agent and stat_queue and function for fill_answered_calls

--- datastorage/asterisk.sql
+++ datastorage/asterisk.sql
@@ -2431,12 +2431,14 @@ CREATE TABLE "stat_agent" (
  "id" SERIAL PRIMARY KEY,
  "name" VARCHAR(128) NOT NULL
 );
+CREATE INDEX stat_agent__idx_name ON stat_agent("name");
 
 DROP TABLE IF EXISTS "stat_queue";
 CREATE TABLE "stat_queue" (
  "id" SERIAL PRIMARY KEY,
  "name" VARCHAR(128) NOT NULL
 );
+CREATE INDEX stat_queue__idx_name ON stat_queue("name");
 
 DROP TABLE IF EXISTS "stat_call_on_queue";
 CREATE TABLE "stat_call_on_queue" (
@@ -2775,6 +2777,32 @@ CREATE TYPE "queue_statistics" AS (
     max_hold_time integer,
     mean_hold_time integer
 );
+
+DROP FUNCTION IF EXISTS "fill_answered_calls" (text, text);
+CREATE FUNCTION "fill_answered_calls"(period_start text, period_end text)
+  RETURNS void AS
+$$
+  INSERT INTO stat_call_on_queue (callid, "time", talktime, waittime, queue_id, agent_id, status)
+  SELECT
+    callid,
+    CAST ((SELECT "time" FROM queue_log WHERE callid=outer_queue_log.callid and event='ENTERQUEUE') AS TIMESTAMP) AS "time",
+    CASE WHEN event IN ('COMPLETEAGENT', 'COMPLETECALLER') THEN CAST (data2 AS INTEGER)
+         WHEN event = 'TRANSFER' THEN CAST (data4 AS INTEGER) END as talktime,
+    CASE WHEN event IN ('COMPLETEAGENT', 'COMPLETECALLER') THEN CAST (data1 AS INTEGER)
+         WHEN event = 'TRANSFER' THEN CAST (data3 AS INTEGER) END as waittime,
+    (SELECT id FROM stat_queue WHERE "name"=queuename) AS queue_id,
+    (SELECT id FROM stat_agent WHERE "name"=agent) AS agent_id,
+    'answered' AS status
+  FROM
+    queue_log as outer_queue_log
+  WHERE
+    callid IN
+      (SELECT callid
+       FROM queue_log
+       WHERE event = 'ENTERQUEUE' AND "time" BETWEEN $1 AND $2)
+    AND event IN ('COMPLETEAGENT', 'COMPLETECALLER', 'TRANSFER');
+$$
+LANGUAGE SQL;
 
 DROP FUNCTION IF EXISTS "fill_saturated_calls" (text, text);
 CREATE FUNCTION "fill_saturated_calls"(period_start text, period_end text)
