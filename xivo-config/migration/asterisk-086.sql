421cb6379ee7df82ce973d13a51e1cae6484c64a
Cedric Abunar <cabunar@avencall.com>
2012-11-27 15:14:24 -0500

    [clean]remove unused dundi table

---
+++ xivo-config/migration/asterisk-086.sql
@@ -0,0 +1,25 @@
+/*
+ * XiVO BASe-Config
+ * Copyright (C) 2012  Avencall
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License AS published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+BEGIN;
+
+DROP TABLE IF EXISTS "dundi" CASCADE;
+DROP TABLE IF EXISTS "dundi_mapping" CASCADE;
+DROP TABLE IF EXISTS "dundi_peer" CASCADE;
+
+COMMIT;
