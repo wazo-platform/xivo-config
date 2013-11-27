ba32299d1d3df68b683217b0c64a0eb176775992
Pascal Cadotte Michaud <pcadottemichaud@avencall.com>
2013-11-27 09:36:10 -0500

    add columns disallow and allow in sccpline

--- xivo-config/datastorage/asterisk.sql
+++ xivo-config/datastorage/asterisk.sql
@@ -2619,6 +2619,8 @@ CREATE TABLE "sccpline" (
     "context"   varchar(80) NOT NULL,
     "cid_name"  varchar(80) NOT NULL,
     "cid_num"   varchar(80) NOT NULL,
+    "disallow"  varchar(100) NOT NULL DEFAULT '',
+    "allow"     text,
     "protocol" "trunk_protocol" NOT NULL DEFAULT 'sccp',
     "commented" INTEGER NOT NULL DEFAULT 0,
     PRIMARY KEY("id")
