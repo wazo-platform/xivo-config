3934aa92dca30f7c7359d18757f3e23a03d1a477
Vincent Menard <vmenard@wazo.io>
2021-11-23 09:54:28 -0500

    remove queue_info related cron

--- debian/xivo-config.cron.d
+++ debian/xivo-config.cron.d
@@ -2,4 +2,3 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 */5 *   * * *   root       xivo-check-dahdi-ports
 */5 *   * * *   root       xivo-check-long-calls
-0   */3 * * *   postgres   xivo-strip-queue-info 10800 > /dev/null
