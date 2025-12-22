3ad1038ccb2bf756a0e6d05b731c1a5b4182c9f9
Francois Blackburn <fblackburn@wazo.community>
2025-12-22 13:33:35 -0500

    odbc script: remove ODBC cleanup section
    why: this section was previously shipped by wazo with configuration
    (like 10 years before ...). So this code was "migration" code part to
    remove this modification. This is now not used anymore

--- tests/test_xivo_update_odbcinst_config.py
+++ tests/test_xivo_update_odbcinst_config.py
@@ -1,4 +1,4 @@
-# Copyright 2015-2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from textwrap import dedent
@@ -43,30 +43,6 @@ def test_modified_comm_log(update_odbcinst_config):
             call().__enter__(),
             call().write('[PostgreSQL ANSI]\n'),
             call().write('CommLog = 0\n'),
-            call().write('\n'),
-            call().__exit__(None, None, None),
-        ]
-    )
-
-
-def test_removes_pooling_and_odbc(update_odbcinst_config):
-    config = """
-        [xivo]
-
-        [ODBC]
-        Pooling = 1
-    """
-    update_odbcinst_config.open = mock_open(read_data=dedent(config))
-    update_odbcinst_config.main()
-    update_odbcinst_config.open.assert_has_calls(
-        [
-            call(CONFIG_FILE_PATH),
-            call().__enter__(),
-            call().__getattr__('__iter__')(),
-            call().__exit__(None, None, None),
-            call(CONFIG_FILE_PATH, 'w'),
-            call().__enter__(),
-            call().write('[xivo]\n'),
             call().write('\n'),
             call().__exit__(None, None, None),
         ]
